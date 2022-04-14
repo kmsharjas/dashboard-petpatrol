9afd7f6e2ba26c2dd61bf035c04417e4c9172a16
SHARJAS <sharjas.k.m@gmail.com>
2022-04-14 18:53:43 +0530

    dashboard kanthari -> dashboard petpatrol

--- karma.conf.js
+++ karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/dashboard-kanthari'),
+      dir: require('path').join(__dirname, './coverage/dashboard-petpatrol'),
       subdir: '.',
       reporters: [
         { type: 'html' },
